ffa67f0dcb67f95110fe3aed035ea83071461333
Isabelly1 <isabellymartins846@gmail.com>
2022-09-23 15:09:01 -0300

    inseri a header com menu

--- src/Components/Header/Styles.js
+++ src/Components/Header/Styles.js
@@ -1,5 +1,43 @@
 import styled from "styled-components";
+import back from "../../assets/back.png"
+
+
 
 export const StyleHeader = styled.div`
-  color: green;
+  background-image: url(${back});
+  background-repeat:no-repeat;
+  background-size:100%;
+  color: #373737;
+  width:100%;
+  height: 120vh;
+
+`;
+
+export const Menu = styled.div`
+border: solid red;
+display:flex;
+justify-content: space-between;
+
+`;
+
+export const Nav = styled.nav`
+border: solid red;
+display:flex;
+justify-content: space-between;
+width:50%;
+`;
+
+
+export const Ul = styled.ul`
+border:solid;
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  width: 40rem;
+`;
+
+export const Li = styled.li`
+width: 11rem;
+font-size: 1.3rem;
+
 `;
